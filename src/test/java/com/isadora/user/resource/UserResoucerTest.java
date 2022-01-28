6769e4d8733ac52d4bbf2e991ae37369c511435b
Isadora Palhoti <isadorapalhoti@gmail.com>
2022-01-28 20:25:16 -0300

    Ciracao dos teste de UserResoucer

---
+++ src/test/java/com/isadora/user/resource/UserResoucerTest.java
@@ -0,0 +1,138 @@
+package com.isadora.user.resource;
+
+import com.isadora.user.domain.User;
+import com.isadora.user.domain.dto.UserDto;
+import com.isadora.user.services.impl.UserServiceImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+class UserResoucerTest {
+
+    public static final Integer ID = 1;
+    public static final String NAME = "Isadora";
+    public static final String EMAIL = "isadora@mail.com";
+    public static final String PASSWORD = "123";
+    public static final String ID_NOT_FOUND = "ID NOT FOUND";
+    public static final String E_MAIL_JA_CADASTRADO_NO_SISTEMA = "E-mail já cadastrado no sistema";
+
+    @InjectMocks
+    private UserResoucer userResoucer;
+
+    @Mock
+    private UserServiceImpl userService;
+
+    @Mock
+    private ModelMapper mapper;
+
+    private User user;
+    private UserDto userDto;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        startUser();
+    }
+
+    @Test
+    void whenFindByIdThenReturnSuccess() {// findById
+        when(userService.findById(anyInt())).thenReturn(user);
+        when(mapper.map(any(), any())).thenReturn(userDto);
+
+        ResponseEntity<UserDto> response = userResoucer.findById(ID);
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(UserDto.class, response.getBody().getClass());
+
+        assertEquals(ID, response.getBody().getId());
+        assertEquals(NAME, response.getBody().getName());
+        assertEquals(EMAIL, response.getBody().getEmail());
+        assertEquals(PASSWORD, response.getBody().getPassword());
+    }
+
+    @Test
+    void whenFindAllThenReturnAListOfUserDTO() { // findAll
+        when(userService.findAll()).thenReturn(List.of(user));
+        when(mapper.map(any(), any())).thenReturn(userDto);
+
+        ResponseEntity<List<UserDto>> response = userResoucer.findAll();
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(ArrayList.class, response.getBody().getClass());
+        assertEquals(UserDto.class, response.getBody().get(0).getClass());
+
+        assertEquals(ID, response.getBody().get(0).getId());
+        assertEquals(NAME, response.getBody().get(0).getName());
+        assertEquals(EMAIL, response.getBody().get(0).getEmail());
+        assertEquals(PASSWORD, response.getBody().get(0).getPassword());
+    }
+
+    @Test
+    void  whenCreateThenReturnCreated() {// create
+        when(userService.create(any())).thenReturn(user);
+
+        ResponseEntity<UserDto> response = userResoucer.create(userDto);
+
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        assertNotNull(response.getHeaders().get("Location"));
+    }
+
+    @Test
+    void whenUpdateThenReturnSuccess() {// update
+        when(userService.update(userDto)).thenReturn(user);
+        when(mapper.map(any(), any())).thenReturn(userDto);
+
+        ResponseEntity<UserDto> response =  userResoucer.update(ID,userDto);
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(UserDto.class, response.getBody().getClass());
+
+        assertEquals(ID, response.getBody().getId());
+        assertEquals(NAME, response.getBody().getName());
+        assertEquals(EMAIL, response.getBody().getEmail());
+    }
+
+    @Test
+    void whenDeleteThenReturnSuccess() {// delete
+        doNothing().when(userService).delete(anyInt());
+
+        ResponseEntity<UserDto> response = userResoucer.delete(ID);
+
+        assertNotNull(response);
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
+        verify(userService, times(1)).delete(anyInt());
+    }
+
+    private void startUser(){
+        user = new User(ID, NAME, EMAIL, PASSWORD);
+        userDto = new UserDto(ID, NAME, EMAIL, PASSWORD);
+    }
+}
