4bfc2dc5545112018cefb68667f7d5052f3423ab
Isadora Palhoti <isadorapalhoti@gmail.com>
2022-01-31 15:50:11 -0300

    Ciracao dos teste de ResourceEcxeptionHandler

---
+++ src/test/java/com/isadora/user/resource/exceptions/ResourceEcxeptionHandlerTest.java
@@ -0,0 +1,58 @@
+package com.isadora.user.resource.exceptions;
+
+import com.isadora.user.services.exceptions.DataIntegratyViolationException;
+import com.isadora.user.services.exceptions.ObjectNotFoundException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ResourceEcxeptionHandlerTest {
+    public static final String ID_NOT_FOUND = "ID NOT FOUND";
+    public static final String E_MAIL_JA_CADASTRADO_NO_SISTEMA = "E-mail já cadastrado no sistema";
+
+    @InjectMocks
+    private ResourceEcxeptionHandler ecxeptionHandler;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void whenObjectNotFoundExceptionThenReturnAResponseEntity() { // objectNotFound
+        ResponseEntity<StandardError> response = ecxeptionHandler
+                .objectNotFound(new ObjectNotFoundException(ID_NOT_FOUND),new MockHttpServletRequest());
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(StandardError.class, response.getBody().getClass());
+        assertEquals(ID_NOT_FOUND, response.getBody().getError());
+        assertEquals(404, response.getBody().getStatus());
+    }
+
+    @Test
+    void dataIntegrityViolationException() {
+        ResponseEntity<StandardError> response = ecxeptionHandler
+                .dataIntegrityViolationException(
+                        new DataIntegratyViolationException(E_MAIL_JA_CADASTRADO_NO_SISTEMA),
+                        new MockHttpServletRequest());
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(StandardError.class, response.getBody().getClass());
+        assertEquals(E_MAIL_JA_CADASTRADO_NO_SISTEMA, response.getBody().getError());
+        assertEquals(400, response.getBody().getStatus());
+    }
+}
